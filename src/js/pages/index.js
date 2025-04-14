d9b4d69b1d99a143d987cdc5802ab19aee43e4ca
georges <georges@lajungle.fr>
2025-04-14 10:55:56 +0200

    intro anim + carte

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -2,10 +2,30 @@ import '../../styles/index.scss'
 import '../../styles/pages/index.scss'
 import Lenis from 'lenis';
 
-import GL from '../components/GL.js';
+// import GL from '../components/GL.js';
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default class Index {
   constructor(options) {
+    this.SmoothScroll();
+    this.Intro();
+  }
+
+
+  Intro() {
+    document.body.scrollTo(0, 0);
+    const intro = document.querySelector('.intro')
+    intro.style.pointerEvents = 'auto';
+    intro.classList.add('play-anim')
+
+  }
+
+  SmoothScroll() {
+    setTimeout(() => {
     this.lenis = new Lenis();
     this.lenis.on('scroll', (e) => {
     })
@@ -14,10 +34,10 @@ export default class Index {
       requestAnimationFrame(raf)
     }
     requestAnimationFrame(raf)
-    this.gl = new GL();
-    this.lenis.on('scroll', (e) => {
-      this.gl.onScroll(e)
-    })
+    // this.gl = new GL();s
+    // this.lenis.on('scroll', (e) => {
+    //   this.gl.onScroll(e)
+    // })
     const items = document.querySelectorAll('#list-section li');
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
@@ -30,8 +50,26 @@ export default class Index {
     items.forEach(item => {
       observer.observe(item);
     });
+
+    document.body.classList.add('loaded');
+  }, 1000);
   }
+  
 }
 window.addEventListener('load', () => {
+  gsap.to("#logo-group", {
+    scale: 1,
+    transformOrigin: "center center",
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+      onLeave: () => {
+        document.querySelector('.intro').style.pointerEvents = 'none';
+      }
+    }
+  });
   new Index();
 });
